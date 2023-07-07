f275ebf85b155afa46e42fb626e5c261219047a2
Alex <advocat6787428@gmail.com>
2023-07-07 19:12:17 +0200

    Seminar

---
+++ task1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number1 = int.Parse(Console.ReadLine());
+if (number1 == number2 * number2)
+{
+   Console.WriteLine($"Число {number1} является квадратом числа " + number2);
+}
+else
+   Console.WriteLine($"Число {number1} не является квадратом числа " + number2);
+
